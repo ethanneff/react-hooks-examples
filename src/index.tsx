f776a3b6f84637d956fbe50ab0c465588a5976dd
Ethan Neff <ethan.neff@eneff.com>
2019-06-16 21:47:47 -0700

    fix: add basic boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Sample } from "./screens";
+import * as serviceWorker from "./serviceWorker";
+ReactDOM.render(<Sample />, document.getElementById("root"));
+serviceWorker.register();
